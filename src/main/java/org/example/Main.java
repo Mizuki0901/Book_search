325ad7f472c1e01ca6b11d344b02d51a1374e68a
Mizuki Kobayashi <umizu.51591@gmail.com>
2024-06-15 21:55:26 +0900

    ９行目の削除

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
+
     Library library = new Library();
 
-    Search search = new Search();
     Search.search();
   }
 }
